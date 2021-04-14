63b2ad84a407e9bd36bc12aff89df1b79de2e19e
dtandazo <dtandazo@pichincha.com>
2021-04-14 16:36:35 -0500

    feat: delete autherizathion - apigeee manage.

--- src/main/java/com/dtandazo/devops/assessment/controller/GreetingRestController.java
+++ src/main/java/com/dtandazo/devops/assessment/controller/GreetingRestController.java
@@ -5,9 +5,6 @@ import com.dtandazo.devops.assessment.model.request.GreetingDevOps;
 import com.dtandazo.devops.assessment.model.response.MessageGreet;
 import com.dtandazo.devops.assessment.service.GreetingService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -31,10 +28,6 @@ public class GreetingRestController {
             content = @Content(schema = @Schema(implementation = MessageGreet.class))),
         @ApiResponse(responseCode = "401", description = "Unauthorized"),
         @ApiResponse(responseCode = "405", description = "ERROR")})
-    @Parameters(value = {
-        @Parameter(in = ParameterIn.HEADER, name = "X-Parse-REST-API-Key", description = "Api Key", required = true, content = @Content(schema = @Schema(implementation = String.class))),
-        @Parameter(in = ParameterIn.HEADER, name = "X-JWT-KWY", description = "Authorization Bearer", required = true, content = @Content(schema = @Schema(implementation = String.class)))
-    })
     @PostMapping("/DevOps")
     public MessageGreet greet(@RequestBody GreetingDevOps greetingDevOps) {
         return greetingService.getMessage(greetingDevOps);
--- src/main/java/com/dtandazo/devops/assessment/service/TokenService.java
+++
@@ -1,8 +0,0 @@
-package com.dtandazo.devops.assessment.service;
-
-import com.dtandazo.devops.assessment.model.response.User;
-
-public interface TokenService {
-
-    User getToken(String username, String password);
-}
